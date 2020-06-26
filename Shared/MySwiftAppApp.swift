2c7e73d661fb16d9145232dcfc06393907fb038f
Abenx <5195693+rushairer@users.noreply.github.com>
2020-06-26 20:58:43 +0800

    Initial Commit

---
+++ Shared/MySwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftAppApp.swift
+//  Shared
+//
+//  Created by Abenx on 2020/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
